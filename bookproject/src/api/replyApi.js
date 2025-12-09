2544c3f18e95affcb16e8d601014ea676800d862
seoyeong0418 <seoyeong2486@gmail.com>
2025-12-09 13:46:52 +0900

    현우님 수정 [replyApi.js]

--- bookproject/src/api/replyApi.js
+++ bookproject/src/api/replyApi.js
@@ -1,44 +1,32 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:8080", // 백엔드 주소
+    baseURL: "http://localhost:8080",
 });
 
-// ✅ 모든 요청에 Authorization 헤더 자동 추가
-instance.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem("accessToken");
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => Promise.reject(error)
-);
+instance.interceptors.request.use((config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+});
 
 export default instance;
 
-// 댓글 등록
-export const createReply = async (userId, data) => {
-    const response = await instance.post(`/api/boards/reply?userId=${userId}`, data);
-    return response.data;
+// 댓글 목록 조회
+export const fetchReplies = async (boardId) => {
+    const res = await instance.get(`/api/boards/reply/${boardId}`);
+    return res.data.content;
 };
 
-// 댓글 수정
-export const updateReply = async (replyId, data) => {
-    const response = await instance.put(`/api/boards/reply/${replyId}`, data);
-    return response.data;
+// 댓글 생성 (PK 기반)
+export const addReply = async (boardId, content, userId) => {
+    return instance.post(`/api/boards/reply?userId=${userId}`, {
+        boardId,
+        content,
+    });
 };
 
 // 댓글 삭제
-export const fetchBookDetail = async (replyId) => {
-    const response = await instance.get(`/api/boards/reply/${replyId}`);
-    return response.data;
-};
-
-// 댓글 목록 조회
-// 게시판 아이디에 해당하는 댓글 목록 조회
-export const fetchReplies = async (boardId) => {
-    const response = await instance.get(`/api/boards/reply/${boardId}`);
-    return response.data.content; // Page 객체의 content 배열만 사용
+export const deleteReply = async (replyId) => {
+    return instance.delete(`/api/boards/reply/${replyId}`);
 };
